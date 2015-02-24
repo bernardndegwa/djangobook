c84a21d3e8a39e0e3ff59bd2a9d0bfea9b6fcd3f
bernardndegwa <mrbernardndegwa@gmail.com>
2015-02-24 08:07:19 +0300

    First unit test and url mapping with a dummy view

--- book/urls.py
+++ book/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'book.views.home', name='home'),
+    url(r'^$', 'sections.views.home_page', name='home_page'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
