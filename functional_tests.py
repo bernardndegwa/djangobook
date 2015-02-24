30a2b7381b600db1911246c5fe0a8b712be4f98d
bernardndegwa <mrbernardndegwa@gmail.com>
2015-02-24 17:24:23 +0300

    Updated rows

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('sign_in_form')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row.text, [row.text for row in rows])        
+        
     def test_can_start_course_and_retrieve_it_later(self):
             
 #Kamau has heard about a cool way of learning entrepreneurship.
@@ -42,13 +47,10 @@ class NewVisitorTest(unittest.TestCase):
 #When he hits enter, the page updates, and now the page says 
 #welcome Kamau.
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('sign_in_form')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                        any(row.text == 'Kamau' for row in rows),
-                        "Kamau did not appear in table the text is\n%s"%(
-                                                                         table.text,)
-                        )        
+        self.check_for_row_in_list_table('Kamau')
+        
+        self.check_for_row_in_list_table('Kamau')
+        
 #He also notices that the page has courses with a short text
 #description listed on the page.The courses spread all the
 #way to the footer of the page.
