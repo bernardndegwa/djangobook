247addb586f982bc3a0f9fae0383fbe55d4cb632
bernardndegwa <mrbernardndegwa@gmail.com>
2015-02-24 09:40:05 +0300

    Refactor home page to use templates

--- sections/views.py
+++ sections/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+#from django.http.response import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>Learn Interactively</title></html>')
+    return render(request, 'home.html')
